c5c465b86970960710938f7ebeec6f35222b4152
jimako <nsplat@gmail.com>
2018-03-04 19:05:26 +0900

    Added initial commit.

---
+++ docker_run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+THIS_SCRIPT_DIR=$(cd $(dirname $0); pwd)
+CONTAINER_LOGON_DIR=/root
+docker run -v ${THIS_SCRIPT_DIR}/home:${CONTAINER_LOGON_DIR} -p 127.0.0.1:5900:5900 -p 127.0.0.1:5901:5901 -w ${CONTAINER_LOGON_DIR} -itd jimako1989/ubuntu-avs_sdk
