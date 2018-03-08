42e284d73acf9e3240bf520b5ecf445d75986cf4
jimako <nsplat@gmail.com>
2018-03-08 23:56:59 +0900

    Modified docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -4,4 +4,4 @@ CONTAINER_LOGON_DIR=/root/workspace
 echo "Host dir:"${THIS_SCRIPT_DIR}/workspace" Client dir:"${CONTAINER_LOGON_DIR}
 echo "Launching the server... This may take a few minutes.."
 chmod +x ${THIS_SCRIPT_DIR}/workspace/entrypoint.sh
-docker run -v ${THIS_SCRIPT_DIR}/workspace:${CONTAINER_LOGON_DIR} -p 6080:80 -it jimako1989/ubuntu-avs_sdk
+docker run -v ${THIS_SCRIPT_DIR}/workspace:${CONTAINER_LOGON_DIR} -p 6080:80 -it ubuntu-avs_sdk
