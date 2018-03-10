b16de4188355d5e9ec8aaadcae331cf836c21025
jimako <nsplat@gmail.com>
2018-03-10 13:45:04 +0900

    Modified Dockerfile to remove pwd

--- workspace/entrypoint.sh
+++ workspace/entrypoint.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 cd /home/root/sdk-folder/sdk-build && python AuthServer/AuthServer.py
--- workspace/run_sample.sh
+++ workspace/run_sample.sh
@@ -1,2 +1,3 @@
+#!/bin/sh
 cd /home/root/sdk-folder/sdk-build/SampleApp/src
 ./SampleApp /home/root/sdk-folder/sdk-build/Integration/AlexaClientSDKConfig.json /home/root/sdk-folder/third-party/alexa-rpi/models
